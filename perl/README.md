9d6e504b03389de2df27acc7390901e7cff5937f
Crestwave <crest.wave@yahoo.com>
2019-04-19 18:18:37 +0800

    Add Perl 5 implementation

---
+++ perl/README.md
@@ -0,0 +1,12 @@
+# bf.pl
+
+A brainfuck interpreter written in pure Perl 5
+
+## Info
+
+- The program is transpiled into Perl 5 and evaluated
+- An array of 30,000 cells in the positive direction is initialized
+	- Cells are initialized when modified
+	- Printing uninitialized cells is not allowed
+	- Negative pointers refer to cells from the end of the array
+- Output is ASCII-encoded
---
+++ perl/bf.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my @a = (0) x 30000;
+my $p = 0;
+my $c;
+my %translation = (
+    '>' => '++$p',
+    '<' => '--$p',
+    '+' => '++$a[$p]; $a[$p] = 0 if ($a[$p] > 255)',
+    '-' => '--$a[$p]; $a[$p] = 255 if ($a[$p] < 0)',
+    '.' => 'print chr($a[$p])',
+    ',' => '$c = getc and $a[$p] = ord($c)',
+    '[' => 'while ($a[$p]) {',
+    ']' => '}',
+);
+
+local $/;
+our $_ = <>;
+die $! unless defined;
+s/[^><+-.,[\]]//g;
+s/./$translation{$&};/g;
+eval;
+die $@ if $@;
