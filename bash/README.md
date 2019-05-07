08799b25caa148a327672193decfe90ae01a029a
Crestwave <crest.wave@yahoo.com>
2019-05-07 21:47:23 +0800

    Use arrays instead in bf.bash

--- bash/README.md
+++ bash/README.md
@@ -6,10 +6,10 @@ A brainfuck interpreter written in pure Bash
 
 - It reads the program from a file named in the first argument
 	- If there are no arguments, it reads from standard input
-- It only depends on Bash 4+ and is portable
-- It does not optimize on specific patterns
+- It only depends on Bash 4.3+ and is portable
+- An array with the length of 30,000 cells is created
+	- Negative pointers count from the back of the array
 - Unmatched brackets are detected during runtime
-- Negative pointers are allowed
 - Output is ASCII-encoded
 
 
@@ -17,3 +17,4 @@ A brainfuck interpreter written in pure Bash
 
 - The program is transpiled into Bash then evaluated
 - It reads from files named in all arguments
+- Negative pointers are allowed
--- bash/bf.bash
+++ bash/bf.bash
@@ -20,9 +20,9 @@ else
 fi
 
 program=${program//[^'><+-.,[]']}
-declare -A tape
+(( tape[29999] = ptr = 0 ))
 
-for (( i = ptr = 0; i < ${#program}; ++i )); do
+for (( i = 0; i < ${#program}; ++i )); do
 	case ${program:i:1} in
 		'>')
 			(( ++ptr ))
@@ -31,26 +31,25 @@ for (( i = ptr = 0; i < ${#program}; ++i )); do
 			(( --ptr ))
 			;;
 		'+')
-			(( tape['$ptr'] = tape['$ptr']+1 & 255 ))
+			(( tape[ptr] = tape[ptr]+1 & 255 ))
 			;;
 		'-')
-			(( tape['$ptr'] = tape['$ptr']-1 & 255 ))
+			(( tape[ptr] = tape[ptr]-1 & 255 ))
 			;;
 		'.')
-			printf -v f %x "${tape[$ptr]}"
+			printf -v f %x "${tape[ptr]}"
 			printf %b "\x$f"
 			;;
 		',')
 			[[ -z $REPLY ]] && read -r && REPLY+=$'\n'
 
 			[[ $REPLY ]] && {
-				LC_CTYPE=C printf -v f %d "'${REPLY::1}"
-				tape[$ptr]=$f
+				printf -v 'tape[ptr]' %d "'${REPLY::1}"
 				REPLY=${REPLY:1}
 			}
 			;;
 		'[')
-			if (( tape['$ptr'] )); then
+			if (( tape[ptr] )); then
 				stack+=("$i")
 			else
 				for (( depth = 1; depth > 0 && ++i; )); do
@@ -63,15 +62,15 @@ for (( i = ptr = 0; i < ${#program}; ++i )); do
 			fi
 			;;
 		']')
-			(( _ = ${#stack[@]} )) || die "unmatched ]"
+			(( ${#stack[@]} )) || die "unmatched ]"
 
-			if (( tape['$ptr'] )); then
-				(( i = stack[_-1] ))
+			if (( tape[ptr] )); then
+				(( i = stack[-1] ))
 			else
-				unset 'stack[_-1]'
+				unset 'stack[-1]'
 			fi
 			;;
 	esac
 done
 
-(( ${#stack[@]} == 0 )) || die "unmatched ["
+(( ! ${#stack[@]} )) || die "unmatched ["
