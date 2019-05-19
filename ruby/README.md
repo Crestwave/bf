0ead1437a2e461f894dd20d029d2eb7455b6896c
Crestwave <crest.wave@yahoo.com>
2019-05-19 18:26:51 +0800

    bf2.rb: initial import

--- ruby/README.md
+++ ruby/README.md
@@ -8,3 +8,8 @@ A brainfuck interpreter written in pure Ruby
 - An array of 30,000 cells is initialized
 	- Negative pointers refer to cells from the end of the array
 - Output is ASCII-encoded
+
+## bf2.rb Info
+
+- `eval` is not used
+- Negative pointers are allowed
---
+++ ruby/bf2.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+program = ARGF.read
+len = program.length
+
+stack = []
+jumps = []
+
+program.each_char.with_index do |c, i|
+  if c == '['
+    stack.push(i)
+  elsif c == ']'
+    j = stack.pop
+    jumps[i] = j
+    jumps[j] = i
+  end
+end
+
+tape = Hash.new(0)
+ptr = 0
+i = 0
+
+while i < len
+  case program[i]
+  when '>'
+    ptr += 1
+  when '<'
+    ptr -= 1
+  when '+'
+    tape[ptr] = (tape[ptr] + 1) & 255
+  when '-'
+    tape[ptr] = (tape[ptr] - 1) & 255
+  when '.'
+    putc tape[ptr].chr
+  when ','
+    tape[ptr] = $stdin.getbyte unless $stdin.eof?
+  when '['
+    i = jumps[i] if tape[ptr].zero?
+  when ']'
+    i = jumps[i] if tape[ptr].nonzero?
+  end
+  i += 1
+end
