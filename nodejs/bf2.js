ee27d44702dcaea043935fe5056a87f47e69ec7c
Crestwave <crest.wave@yahoo.com>
2019-09-11 14:27:26 +0800

    bf2.js: initial import

---
+++ nodejs/bf2.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+let program;
+const fs = require('fs');
+if (process.argv.length > 2)
+    process.argv.slice(2, process.argv.length).forEach(arg => {
+        program += fs.readFileSync(arg, 'ascii');
+    });
+else
+    program = fs.readFileSync(0, 'ascii');
+
+program = program.replace(/[^><+\-.,[\]]/g, '');
+const len = program.length;
+let a = [];
+let p = 0;
+let buf = new Int8Array(1);
+
+const translation = {
+    '>': '++p;',
+    '<': '--p;',
+    '+': 'a[p] = (a[p]||0)+1 & 255;',
+    '-': 'a[p] = (a[p]||0)-1 & 255;',
+    '.': 'process.stdout.write(String.fromCharCode(a[p]));',
+    ',': 'if (fs.readSync(0, buf, 0, 1)) a[p] = buf[0];',
+    '[': 'while (a[p]) {',
+    ']': '}'
+}
+program = program.replace(/./g, c => translation[c]);
+eval(program);
