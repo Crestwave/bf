eceaec075b7aaf3a380e026196d3928c460103e5
Crestwave <crest.wave@yahoo.com>
2021-08-11 13:09:43 +0800

    bf.swift: feature-complete Swift 5 version

---
+++ swift/README.md
@@ -0,0 +1,12 @@
+# bf.swift
+
+A brainfuck interpreter written in pure Swift 5
+
+## Info
+
+- It reads the program from a file named in the first argument
+	- If there are no arguments, it reads from standard input
+- An array of 30,000 cells is initialized
+	- The pointer wraps around when it goes out of range
+- Unmatched brackets are detected before runtime
+- Output is UTF-8-encoded
--- swift/bf.swift
+++ swift/bf.swift
@@ -1,61 +1,50 @@
 #!/usr/bin/env swift
+import Foundation
 
-/*
-let line = readLine(strippingNewline: false)
-if line != nil {
-    let l = line!
-    let c = l[l.index(l.startIndex, offsetBy: 0)]
-    print(c)
-    print(l, terminator: "")
-    //print(l)
-    //print(line![line!.index(line!.startIndex, offsetBy: 0)])
-} else {
-    print("EOF")
-}
-*/
-
-
+let stderr = FileHandle.standardError
 var program = ""
-while let line = readLine() {
-    program.append(line)
-}
-//var program = ",[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>++++++++++++++<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>>+++++[<----->-]<<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>++++++++++++++<-[>+<-[>+<-[>+<-[>+<-[>+<-[>++++++++++++++<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>>+++++[<----->-]<<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>++++++++++++++<-[>+<-]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]>.[-]<,][..]"
-//print(program)
-//print(program.characters.count)
-//var jumps: [Int] = []
-var jumps = Array(repeating: 0, count: program.characters.count)
-var stack: [Int] = []
-//program.characters.enumerated().forEach { print($0, ":", $1) }
-//jumps.reserveCapacity(program.characters.count)
-//stack.reserveCapacity(program.characters.count)
-
-for (i, c) in program.characters.enumerated() {
-    if c == "[" {
-        stack.append(i)
-    } else if c == "]" {
-        let j = stack.removeLast()
-        //jumps.insert(i, at: j)
-        jumps[i] = j
-        jumps[j] = i
-        //jumps.insert(j, at: i)
-        //print(j, ":", i)
+if CommandLine.argc > 1 {
+    do {
+        program = try String(contentsOfFile: CommandLine.arguments[1])
+    } catch let error as NSError {
+        let msg = "\(CommandLine.arguments[1]): \(error.localizedDescription)\n"
+        stderr.write(msg.data(using: .utf8)!)
+        exit(1)
+    }
+} else {
+    while let line = readLine() {
+        program.append(line)
     }
 }
 
-/*
-for (i, c) in jumps.enumerated() {
-    print(i, ": ", c)
-}
-*/
+var jumps = Array(repeating: 0, count: program.count)
+var stack: [Int] = []
 
+for (i, c) in program.enumerated() {
+    if c == "[" {
+        stack.append(i)
+    } else if c == "]" {
+        if stack.count == 0 {
+            stderr.write("error: extraneous ']'\n".data(using: .utf8)!)
+            exit(1)
+        }
+
+        let j = stack.removeLast()
+        jumps[i] = j
+        jumps[j] = i
+    }
+}
+
+if stack.count > 0 {
+    stderr.write("error: expected ']'\n".data(using: .utf8)!)
+    exit(1)
+}
+
+let len = program.count
+var line: String = ""
 var ptr = 0
 var tape = Array(repeating: 0, count: 30000)
-let len = program.characters.count
-var line: String = ""
 
-//for (i, c) in program.characters.enumerated() {
-//for i in stride(from: 0, to: len, by: 1) {
-//for i in sequence(first: 0, next: {$0 + 1}).prefix(while: {$0 < len}) {
 var i = 0
 while (i < len) {
     let c = program[program.index(program.startIndex, offsetBy: i)]
@@ -82,16 +71,15 @@ while (i < len) {
     } else if c == "." {
         print(Character(UnicodeScalar(tape[ptr])!), terminator: "")
     } else if c == "," {
-        if line.characters.count == 0 {
+        if line.count == 0 {
             let input = readLine(strippingNewline: false)
             if input != nil {
                 line = input!
             }
         }
-        if line.characters.count > 0 {
-            //let ch = UnicodeScalar(String(line.remove(at: line.startIndex)))!.value
-            //tape[ptr] = Int(ch)
-            tape[ptr] = Int(UnicodeScalar(String(line.remove(at: line.startIndex)))!.value)
+        if line.count > 0 {
+            tape[ptr] = Int(UnicodeScalar(String(line
+                            .remove(at: line.startIndex)))!.value)
         }
     } else if c == "[" {
         if tape[ptr] == 0 {
@@ -104,10 +92,4 @@ while (i < len) {
     }
 
     i += 1
-    //print(i, ":", tape[ptr])
 }
-//print(CommandLine.argc)
-//let arguments = CommandLine.arguments
-//for i in arguments {
-//    print(i)
-//}
